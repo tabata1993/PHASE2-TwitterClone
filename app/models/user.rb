cfb61b35e96b1138742957bd9c7fd9d1a004a965
田畑智士 <tabatasatoshi1993@gmail.com>
2018-07-04 17:48:30 +0900

    画像機能用にCSSを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :favorite_narrates, through: :favorites , source: :narrate
 
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 255},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_save { email.downcase! }
   has_secure_password
